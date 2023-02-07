4164369cdf18aab1d7493ca3856d83bf2c150180
C0Florent <florent.charpentier@epitech.eu>
2023-02-07 09:32:51 +0100

    [ :books: :art: ] :arrow_double_up: move_cursor( ) now receives the sfEvent by pointer

--- lib/my_graphics/slidebar/move_cursor.c
+++ lib/my_graphics/slidebar/move_cursor.c
@@ -9,41 +9,41 @@
 #include <SFML/Graphics/RectangleShape.h>
 #include "my_graphics.h"
 
-static bool is_click_on_cursor(slidebar_t const *sb,
-sfMouseButtonEvent mouse_pos)
+static bool is_click_on_cursor(slidebar_t *sb,
+sfMouseButtonEvent const *mouse_pos)
 {
-    if (mouse_pos.x >= sb->center.x - sb->bar_size.x / 2
-    && mouse_pos.x <= sb->center.x + sb->bar_size.x / 2
-    && mouse_pos.y >= sb->center.y - sb->bar_size.y / 2
-    && mouse_pos.y <= sb->center.y + sb->bar_size.y / 2) {
+    if (mouse_pos->x >= sb->center.x - sb->bar_size.x / 2
+    && mouse_pos->x <= sb->center.x + sb->bar_size.x / 2
+    && mouse_pos->y >= sb->center.y - sb->bar_size.y / 2
+    && mouse_pos->y <= sb->center.y + sb->bar_size.y / 2) {
         return (true);
     } else {
         return (false);
     }
 }
 
-static bool is_able_to_move_cursor(slidebar_t *sb, sfEvent event)
+static bool is_able_to_move_cursor(slidebar_t *sb, sfEvent const *event)
 {
-    if (event.type == sfEvtMouseButtonReleased) {
+    if (event->type == sfEvtMouseButtonReleased) {
         sb->is_grabbed = false;
         return (false);
     }
-    if (event.type == sfEvtMouseButtonPressed) {
-        sb->is_grabbed = is_click_on_cursor(sb, event.mouseButton);
+    if (event->type == sfEvtMouseButtonPressed) {
+        sb->is_grabbed = is_click_on_cursor(sb, &event->mouseButton);
         return (false);
     }
-    if (event.type == sfEvtMouseMoved) {
+    if (event->type == sfEvtMouseMoved) {
         return (sb->is_grabbed);
     }
     return (false);
 }
 
-static bool is_mouse_event(sfEvent event)
+static bool is_mouse_event(sfEvent const *event)
 {
-    switch (event.type) {
+    switch (event->type) {
     case sfEvtMouseButtonPressed:
     case sfEvtMouseButtonReleased:
-        if (event.mouseButton.button == sfMouseLeft) {
+        if (event->mouseButton.button == sfMouseLeft) {
             return (true);
         } else {
             return (false);
@@ -58,7 +58,7 @@ static bool is_mouse_event(sfEvent event)
 
 // This function needs to be called for each frame of rendered image,
 // in order to properly update the sb's cursor position
-void move_cursor(slidebar_t *sb, sfEvent event)
+void move_cursor(slidebar_t *sb, sfEvent const *event)
 {
     if (is_mouse_event(event) == false) {
         return;
