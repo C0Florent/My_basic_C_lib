439799e63c16235c790bb6673a52b550a2bedfe2
C0Florent <florent.charpentier@epitech.eu>
2023-02-07 18:12:57 +0100

    [ :books: :art: ] Added comments in my_graphics.h

--- include/my_graphics.h
+++ include/my_graphics.h
@@ -12,11 +12,15 @@
     #include <SFML/Graphics/RenderWindow.h>
     #include <SFML/System/Vector2.h>
 
-
     #ifndef ABS
         #define ABS(val)    (((val) < 0) ? (-(val)) : (val))
     #endif /* ABS */
 
+
+/* STRUCTURES */
+
+// Structure for easy-to-use sprites, containing both the sprite
+// and the texture in a sinlge object
 typedef struct {
     sfTexture *txtr;
     sfSprite *sprt;
@@ -33,6 +37,7 @@ typedef struct {
 
 
 
+// Create a window with some basic set() for the usual window
 sfRenderWindow *my_create_window(char const *window_name);
 
 sfVector2i vctr2i(int x, int y);
@@ -48,9 +53,16 @@ void destroy_disp_sprt(disp_sprt_t *displayble_sprite);
 
 
 
+
+// Create a slidebar, which will be centered at `pos`, and of size `bar_size`
 slidebar_t *create_slidebar(sfVector2f pos, sfVector2f bar_size);
+
+// Display a slidebar
 void display_slidebar(sfRenderWindow *window, slidebar_t const *slidebar);
+
+// Destroy (free) a slidebar
 void destroy_slidebar(slidebar_t *sb);
+
 
 void slidebar_cursor_set_pos(slidebar_t *sb, float pos);
 void slidebar_cursor_set_pos_from_mouse_pos(slidebar_t * sb,
