cf9fb0237421749d251c08abf33b8fdf6f75a547
C0Florent <florent.charpentier@epitech.eu>
2023-02-13 20:34:12 +0100

    [ :top: ] :heavy_plus_sign: my_macros.h file

---
+++ include/my_macros.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** My macros
+** File description:
+** Header file containing various macros for all sorts of purposes
+*/
+
+#ifndef MY_MACROS_H_
+    #define MY_MACROS_H_
+
+    #define ABS(val)    (((val) < 0) ? (-(val)) : (val))
+    #define MAX(a, b)   (((a) > (b)) ? (a) : (b))
+    #define MIN(a, b)   (((a) < (b)) ? (a) : (b))
+
+#endif /* !MY_MACROS_H_ */
