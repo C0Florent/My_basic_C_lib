4d24d2d471e0fb42b45bf65edbb59167c33967f4
C0Florent <florent.charpentier@epitech.eu>
2023-02-13 20:47:10 +0100

    [ :books: PRINTF ] :arrow_double_up: Re-done %s properly

--- lib/my_printf/my_put_conv/conv_s.c
+++ lib/my_printf/my_put_conv/conv_s.c
@@ -5,99 +5,37 @@
 ** sub-function for my printf
 */
 
+#include <unistd.h>
 #include <stdarg.h>
 #include "my.h"
 #include "my_printf_internal.h"
+#include "my_macros.h"
 
-static int cut_the_string (conv_det_t *convert, char *str)
+static int get_truncated_str_len(char *str, int precision)
 {
-    for (int i = 0; i < convert->precision; i++) {
-        my_putchar(str[i]);
-    }
-    return 0;
-}
+    int len = my_strlen(str);
 
-int without_dash (conv_det_t *conv, int nb, int num, char *str)
-{
-    if (conv->dash_flag == 0 && conv->width > conv->precision
-        && conv->precision < nb) {
-        put_char_space(num);
-        cut_the_string(conv, str);
+    if (precision >= 0 && precision < len) {
+        return (precision);
+    } else {
+        return (len);
     }
-    if (conv->dash_flag == 0 && conv->width > conv->precision
-        && conv->precision > nb) {
-        put_char_space(num);
-        my_putstr(str);
-    }
-    if (conv->dash_flag == 0 && conv->width <= conv->precision
-        && conv->precision > nb && conv->width >= nb) {
-        put_char_space(conv->width - nb);
-        my_putstr(str);
-    }
-    return 0;
-}
-
-int with_dash (conv_det_t *conv, int nb, int num, char *str)
-{
-    if (conv->dash_flag == 1 && conv->width > conv->precision
-        && conv->precision < nb) {
-            cut_the_string(conv, str);
-            put_char_space(num);
-    }
-    if (conv->dash_flag == 1
-        && conv->width > conv->precision && conv->precision > nb) {
-        my_putstr(str);
-        put_char_space(num);
-    }
-    if (conv->dash_flag == 1 && conv->width <= conv->precision
-        && conv->precision > nb && conv->width >= nb) {
-        my_putstr(str);
-        put_char_space(conv->width - nb);
-    }
-    return 0;
-}
-
-int basics_test(conv_det_t *convert, char *str, int nb)
-{
-    if (convert->dash_flag == 0 && convert->width == 0
-        && convert->precision == -1)
-        my_putstr(str);
-    if (convert->dash_flag == 1 && convert->width != 0
-        && convert->precision == -1) {
-        my_putstr(str);
-        if (nb > 0)
-            put_char_space(nb);
-    }
-    if (convert->dash_flag == 0 && convert->width != 0
-        && convert->precision == -1 && nb > 0) {
-        put_char_space(nb);
-        my_putstr(str);
-    }
-    if (convert->dash_flag == 0 && convert->width != 0
-        && nb < 0 && convert->precision == -1) {
-        my_putstr(str);
-    }
-    return 0;
 }
 
 int my_put_conv_s(conv_det_t *details, va_list *arguments)
 {
-    char *str = va_arg(*arguments, char *);
-    int size_str = my_strlen(str);
-    int size_space = details->width - size_str;
-    int space = details->width - details->precision;
+    char *str = (va_arg(*arguments, char *));
+    int len = get_truncated_str_len(str, details->precision);
+    int spaces_to_print = MAX(details->width - len, 0);
 
-    basics_test(details, str, size_space);
-    if (details->width != 0 && details->precision != -1) {
-        with_dash(details, size_str, space, str);
-        without_dash(details, size_str, space, str);
+    if (details->dash_flag) {
+        write(1, str, len);
     }
-    if (details->width == 0 && details->precision != -1) {
-        if (details->precision < size_str)
-            cut_the_string(details, str);
-        if (details->precision > size_str) {
-            my_putstr(str);
-        }
+    for (int i = 0; i < spaces_to_print; i++) {
+        my_putchar(' ');
     }
-    return 0;
+    if (!details->dash_flag) {
+        write(1, str, len);
+    }
+    return (spaces_to_print + len);
 }
