c50320c9928d2236b1a1f87724a507333bb39ccd
C0Florent <florent.charpentier@epitech.eu>
2023-02-24 14:11:56 +0100

    [ :books: PRINTF ] :sparkles: Implemented `my_vprintf()` function, and updated `my_printf` so that it calls `my_vprintf`
    All my_printf tests are still passing

--- include/my_printf.h
+++ include/my_printf.h
@@ -9,9 +9,10 @@
     #define MY_PRINTF_H
 
     #include <stdarg.h>
-    #include <stdbool.h>
 
 int my_printf(char *format, ...);
+
+int my_vprintf(char *format, va_list *args);
 
 
 #endif /*MY_PRINTF_H*/
--- lib/my_printf/my_printf.c
+++ lib/my_printf/my_printf.c
@@ -9,23 +9,31 @@
 #include "my.h"
 #include "my_printf_internal.h"
 
-int my_printf(char *format, ...)
+int my_vprintf(char *format, va_list *arg)
 {
-    va_list arg;
     conv_det_t conv_details;
     int charcount = 0;
 
-    va_start (arg, *format);
     for (int i = 0; i < my_strlen(format); i++) {
         if (format[i] != '%') {
             my_putchar(format[i]);
             charcount++;
         } else {
-            i += read_conv(&format[i], &conv_details, &arg);
+            i += read_conv(&format[i], &conv_details, arg);
             clean_struct(&conv_details);
-            redirect_to_right_conv(&conv_details, &arg, &charcount);
+            redirect_to_right_conv(&conv_details, arg, &charcount);
         }
     }
+    return (charcount);
+}
+
+int my_printf(char *format, ...)
+{
+    va_list arg;
+    int charcount;
+
+    va_start(arg, *format);
+    charcount = my_vprintf(format, &arg);
     va_end(arg);
     return (charcount);
 }
