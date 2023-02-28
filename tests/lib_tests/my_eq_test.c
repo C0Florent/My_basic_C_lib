cfaceb6f711203ea47881bcc05ace75a82bcfae4
C0Florent <florent.charpentier@epitech.eu>
2023-02-28 15:39:31 +0100

    :test_tube: :heavy_check_mark: Added basic tests for `my_eq()`

---
+++ tests/lib_tests/my_eq_test.c
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2023
+** My eq test
+** File description:
+** Unit tests for my_eq
+*/
+
+#include <criterion/criterion.h>
+#include "newmy.h"
+
+#include "my_slidebar.h"
+#include "my_graphics.h"
+
+Test(my_eq, basic_success)
+{
+    int a = 42;
+    int b = 42;
+    cr_assert(my_eq(&a, &b, sizeof(int)) == true);
+}
+
+Test(my_eq, structure_success)
+{
+    slidebar_t a;
+    slidebar_t b;
+
+    memset(&a, 42, sizeof(slidebar_t));
+    memset(&a + 14, 47, 7);
+    memset(&a, 84, 7);
+
+    memcpy(&b, &a, sizeof(slidebar_t));
+    cr_assert(my_eq(&a, &b, sizeof(slidebar_t)) == true);
+}
+
+Test(my_eq, basic_fail)
+{
+    short a = 42;
+    short b = 84;
+    cr_assert(my_eq(&a, &b, sizeof(short)) == false);
+}
