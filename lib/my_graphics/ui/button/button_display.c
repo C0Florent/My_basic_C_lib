c3f9ebf1643ace7603ebfbe41db1c765a85318f5
C0Florent <florent.charpentier@epitech.eu>
2023-02-28 23:36:10 +0100

    [ :books: :art: ] :sparkles: Function to display buttons

---
+++ lib/my_graphics/ui/button/button_display.c
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2023
+** Display button
+** File description:
+** Function to draw a button on the window's framebuffer
+*/
+
+#include <SFML/Graphics/RectangleShape.h>
+#include <SFML/Graphics/RenderWindow.h>
+#include "my_ui.h"
+
+static void update_button_colour(button_t const *button)
+{
+    switch (button->state) {
+    case NONE:
+        sfRectangleShape_setFillColor(button->rect, sfRed);
+        break;
+    case RELEASED:
+        sfRectangleShape_setFillColor(button->rect, sfWhite);
+        break;
+    case HOVER:
+        sfRectangleShape_setFillColor(button->rect, sfGreen);
+        break;
+    case PRESSED:
+        sfRectangleShape_setFillColor(button->rect, sfYellow);
+        break;
+    }
+}
+
+void button_display(sfRenderWindow *wndw, button_t const *button)
+{
+    update_button_colour(button);
+    sfRenderWindow_drawRectangleShape(wndw, button->rect, NULL);
+}
