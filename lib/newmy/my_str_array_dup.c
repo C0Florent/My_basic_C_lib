0d2cc12bce4ba4403e51fc5e5805c3fa3e225a04
C0Florent <florent.charpentier@epitech.eu>
2023-03-03 10:13:49 +0100

    [ :books: NEWMY ] :arrow_double_up: Re-made entirely `my_str_array_dup`

--- lib/newmy/my_str_array_dup.c
+++ lib/newmy/my_str_array_dup.c
@@ -7,18 +7,52 @@
 
 #include <stdlib.h>
 #include "my.h"
+#include "newmy.h"
 
-char **my_str_array_dup(char **arr, unsigned int nb_rows)
+static void emergency_free(char **arrdup, unsigned int nb_to_free)
 {
-    char **arrdup = malloc(sizeof(char *) * nb_rows + 1);
-
-    if (arrdup == NULL)
-        return (NULL);
-    for (unsigned int i = 0; i < nb_rows; i++) {
-        arrdup[i] = my_strdup(arr[i]);
-        if (arrdup[i] == NULL)
-            return (NULL);
+    for (unsigned int i = 0; i < nb_to_free; i++) {
+        free(arrdup[i]);
     }
-    arrdup[nb_rows] = NULL;
-    return (arrdup);
+    free(arrdup);
+}
+
+static bool dup_rows(char * const *arr, char **arrdup)
+{
+    unsigned int i = 0;
+
+    while (arr[i] != NULL) {
+        arrdup[i] = my_strdup(arr[i]);
+        if (arrdup[i] == NULL) {
+            my_putsterr("str_arr_dup: malloc failed, returning NULL\n");
+            emergency_free(arrdup, i + 1);
+            return (EXIT_FAILURE);
+        }
+        i++;
+    }
+    arrdup[i] = NULL;
+    return (EXIT_SUCCESS);
+}
+
+char **my_str_array_dup(char * const *arr)
+{
+    unsigned int size;
+    char **ret;
+    bool dup_rows_ret;
+
+    if (arr == NULL) {
+        return (NULL);
+    }
+    size = my_str_array_size(arr);
+    ret = malloc(sizeof(char *) * (size + 1));
+    if (ret == NULL) {
+        my_putsterr("str_arr_dup: malloc failed, returning NULL\n");
+        return (NULL);
+    }
+    dup_rows_ret = dup_rows(arr, ret);
+    if (dup_rows_ret == EXIT_FAILURE) {
+        return (NULL);
+    } else {
+        return (ret);
+    }
 }
