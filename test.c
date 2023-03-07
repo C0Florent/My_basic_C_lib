2c0e259d0628dfad5f00feefab7186b01cfc6091
C0Florent <florent.charpentier@epitech.eu>
2023-03-08 00:02:54 +0100

    :art: Updated example program to demonstrate improvements made to buttons

--- test.c
+++ test.c
@@ -9,7 +9,8 @@
 #include "my_ui.h"
 #include <stdio.h>
 
-static void analyse_events(sfRenderWindow *wndw, slidebar_t *sb, button_t *btn)
+static void analyse_events(sfRenderWindow *wndw, slidebar_t *sb,
+button_t *btn1, button_t *btn2, button_t *btn3)
 {
     sfEvent event;
 
@@ -18,7 +19,9 @@ static void analyse_events(sfRenderWindow *wndw, slidebar_t *sb, button_t *btn)
             sfRenderWindow_close(wndw);
         }
         move_cursor(sb, &event);
-        update_button_state(btn, &event);
+        update_button_state(btn1, &event);
+        update_button_state(btn2, &event);
+        update_button_state(btn3, &event);
     }
 }
 
@@ -26,20 +29,26 @@ int main(void)
 {
     sfRenderWindow *wndw = my_create_window("test");
     slidebar_t *sb = create_slidebar(vctr2f(1800, 200), vctr2f(100, 20));
-    button_t *button = button_create(vctr2f(500, 200), vctr2f(45, 45));
+    button_t *button1 = button_create(vctr2f(500, 200), vctr2f(45, 45));
+    button_t *button2 = button_create(vctr2f(575, 200), vctr2f(45, 45));
+    button_t *button3 = button_create(vctr2f(650, 200), vctr2f(45, 45));
 
     slidebar_set_bound_values(sb, 0, 5, KEEP_POS);
     while (sfRenderWindow_isOpen(wndw)) {
         display_slidebar(wndw, sb);
-        button_display(wndw, button);
+        button_display(wndw, button1);
+        button_display(wndw, button2);
+        button_display(wndw, button3);
         printf("\r%f", sb->value);
         fflush(stdout);
         sfRenderWindow_display(wndw);
         sfRenderWindow_clear(wndw, sfColor_fromInteger(0x23272A00));
-        analyse_events(wndw, sb, button);
+        analyse_events(wndw, sb, button1, button2, button3);
     }
     destroy_slidebar(sb);
-    button_destroy(button);
+    button_destroy(button1);
+    button_destroy(button2);
+    button_destroy(button3);
     sfRenderWindow_destroy(wndw);
     printf("\n");
     return (0);
