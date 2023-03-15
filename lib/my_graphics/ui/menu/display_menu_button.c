53e251f4ad4d0516ae76e2167f912b30887cc696
C0Florent <florent.charpentier@epitech.eu>
2023-03-15 17:13:05 +0100

    [ :books :art: ] :heavy_plus_sign: Function to display the menu button

---
+++ lib/my_graphics/ui/menu/display_menu_button.c
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2023
+** MY
+** File description:
+** display_menu_button
+*/
+
+#include <SFML/Graphics/RectangleShape.h>
+#include <SFML/Graphics/RenderWindow.h>
+#include "my_ui.h"
+
+static void update_outline_colour(button_t const *button)
+{
+    switch (button->state) {
+    case RELEASED:
+        sfRectangleShape_setOutlineColor(button->rect, sfWhite);
+        break;
+    case HOVER:
+        sfRectangleShape_setOutlineColor(button->rect, MY_BLUE);
+        break;
+    case PRESSED:
+        sfRectangleShape_setOutlineColor(button->rect, MY_SKYBLUE);
+        break;
+    default:
+    }
+}
+
+static void update_fill_colour(button_t const *button)
+{
+    switch (button->on) {
+    case true:
+        sfRectangleShape_setFillColor(button->rect, MY_MIDGREY);
+        break;
+    case false:
+        sfRectangleShape_setFillColor(button->rect, sfWhite);
+        break;
+    }
+}
+
+void display_menu_button(sfRenderWindow *wndw, button_t const *button)
+{
+    update_outline_colour(button);
+    update_fill_colour(button);
+    sfRenderWindow_drawRectangleShape(wndw, button->rect, NULL);
+}
