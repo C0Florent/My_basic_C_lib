d58678617b1b0d25be7eeebb1c36aebc36e8b582
C0Florent <florent.charpentier@epitech.eu>
2023-03-20 15:37:55 +0100

    [ :top: MACROS ] :banana: Removed trailing spaces on empty line

--- include/my_macros.h
+++ include/my_macros.h
@@ -20,7 +20,7 @@
     #ifndef EXIT_FAILURE
         #define EXIT_FAILURE 1
     #endif /* EXIT_FAILURE */
-    
+
     #ifndef EXIT_SUCCESS
         #define EXIT_SUCCESS 0
     #endif /* EXIT_SUCCESS */
