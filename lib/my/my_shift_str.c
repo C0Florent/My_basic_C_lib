554d80832e0e982e21f2ccfd657c86e5247551cc
C0Florent <florent.charpentier@epitech.eu>
2023-05-12 10:37:05 +0200

    :arrow_double_up: Allowed for negative shift in `my_shift_str`

--- lib/my/my_shift_str.c
+++ lib/my/my_shift_str.c
@@ -5,16 +5,28 @@
 ** If a character string is stored at the beginning of an
 ** array bigger than its length, it might be useful to shift it
 ** few chars forwards in order to add anything at the start of it
+** or backwards to delete the start of the string
 */
 
 #include "my.h"
 
+// If given a positive shift, this function will move byte by
+// byte each character of the string by `shift` bytes forwards, and
+// reset the first `shift` characters of the string to NUL bytes
 int my_shift_str(char *str, int shift)
 {
     int len = my_strlen(str);
 
-    for (int i = len - 1; i >= 0; i--)
-        str[shift + i] = str[i];
-    my_str_initialise(str, shift);
+    if (shift > 0) {
+        for (int i = len - 1; i >= 0; i--) {
+            str[shift + i] = str[i];
+        }
+        my_str_initialise(str, shift);
+    } else {
+        shift = -shift;
+        for (int i = 0; i <= len; i++) {
+            str[i] = str[i + shift];
+        }
+    }
     return (0);
 }
