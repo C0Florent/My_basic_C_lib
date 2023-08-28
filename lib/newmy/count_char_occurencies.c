a5ac96d5f1f7a20c7aa7aa7fcb10fb4ec262c575
C0Florent <florent.charpentier@epitech.eu>
2023-08-28 23:57:35 +0200

    [:books: NEWMY] :banana: Added curly brackets to single-line `if`

--- lib/newmy/count_char_occurencies.c
+++ lib/newmy/count_char_occurencies.c
@@ -10,8 +10,9 @@ int count_occurencies(char const *tested_str, char to_count)
     int count = 0;
 
     for (int i = 0; tested_str[i] != '\0'; i++) {
-        if (tested_str[i] == to_count)
+        if (tested_str[i] == to_count) {
             count++;
+        }
     }
     return (count);
 }
