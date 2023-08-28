1752e748ca455fa4271e9ebc6a6c4b4992cfa787
C0Florent <florent.charpentier@epitech.eu>
2023-08-29 00:17:46 +0200

    [:books: NEWMY] :arrow_double_up: Cleaned up `mega_{put|save}_nbr`

--- lib/newmy/mega_put_nbr.c
+++ lib/newmy/mega_put_nbr.c
@@ -8,27 +8,26 @@
 #include <unistd.h>
 #include "my.h"
 
-static int my_print_nbr_rec(long long nb, unsigned char base, int *count)
+static void my_print_nbr_rec(long long nb, unsigned char base, int *count)
 {
     const char digits[16] = {'0', '1', '2', '3', '4',
     '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-    char sign[2] = {'\0', '\0'};
     long long rec_param = 0;
 
     if (nb < 0) {
-        sign[0] = '-';
+        my_putchar('-');
         nb = -nb;
         (*count)++;
     }
-    my_putstr(sign);
     rec_param = nb / base;
-    if (nb == -9223372036854775807 - 1)
+    if (nb == -9223372036854775807 - 1) {
         rec_param = -rec_param;
-    if (rec_param != 0)
+    }
+    if (rec_param != 0) {
         my_print_nbr_rec(rec_param, base, count);
+    }
     my_putchar(digits[ABS(nb % base)]);
     (*count)++;
-    return (nb % base);
 }
 
 int mega_put_nbr(long long nb, unsigned char base)
