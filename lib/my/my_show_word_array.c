99b053a3e8cc5256dbfc17b610eb403209393170
C0Florent <florent.charpentier@epitech.eu>
2023-08-28 23:10:13 +0200

    [:books:] :banana: Added curly brackets to a single-line `while`

--- lib/my/my_show_word_array.c
+++ lib/my/my_show_word_array.c
@@ -13,8 +13,9 @@ int my_show_word_array(char * const *tab)
     int i;
     int nb_lines = 0;
 
-    while (tab[nb_lines] != 0)
+    while (tab[nb_lines] != 0) {
         nb_lines++;
+    }
     for (i = 0; i < nb_lines; i++) {
         my_putstr(tab[i]);
         my_putchar('\n');
