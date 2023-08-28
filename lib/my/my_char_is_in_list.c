b69b90bf8bcab2823561cb83f92f3a4d83641cca
C0Florent <florent.charpentier@epitech.eu>
2023-08-28 20:47:03 +0200

    [:books:] :label: Changed return type of `my_char_is_in_list` (int -> bool)

--- lib/my/my_char_is_in_list.c
+++ lib/my/my_char_is_in_list.c
@@ -2,15 +2,17 @@
 ** EPITECH PROJECT, 2022
 ** My char is in list
 ** File description:
-** Returns 1 if the given char is somewhere
-** in the given string, otherwise returns 0
+** Returns true if the given char is somewhere
+** in the given string, otherwise returns false
 */
 
-int my_char_is_in_list(char c, char *str)
+#include <stdbool.h>
+
+bool my_char_is_in_list(char c, char *str)
 {
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] == c)
-            return (1);
+            return (true);
     }
-    return (0);
+    return (false);
 }
